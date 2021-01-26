163d0833b6c9518129642ebe7bdc55f4941a3f6b
iam-dev <elky.bachtiar@duo.nl>
2021-01-26 20:31:11 +0100

    Issue 1: fixed deploy contract using constructor values

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
 const Euro = artifacts.require('Euro');
-module.exports = function (deployer) {
-  deployer.deploy(Euro);
+module.exports = function (deployer, network, accounts) {
+  const initialAccount = accounts[0];
+  deployer.deploy(Euro, 'EURO coin', 'EURC', 6, initialAccount, 100000000);
 };
